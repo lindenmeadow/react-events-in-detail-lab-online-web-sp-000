dc7cf07e3ff4f0e779ca707494ea9253c258cdbc
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-15 16:43:48 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -6,7 +6,7 @@ export default class DelayedButton extends Component {
         event.persist();
         setTimeout(() => {
             this.props.onDelayedClick(event);
-        }, 
+        },
             this.props.delay
         );
     }
